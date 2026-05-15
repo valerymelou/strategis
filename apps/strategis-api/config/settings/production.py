8cc42bcb5c3c2c8be0cda19f9dad20f317c0839c
Valery Melou <me@valerymelou.com>
2026-05-15 16:03:19 +0100

    feat: remove Traefik configuration files and add CI/CD workflows for API and Web

--- apps/strategis-api/config/settings/production.py
+++ apps/strategis-api/config/settings/production.py
@@ -22,7 +22,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list(
     "DJANGO_ALLOWED_HOSTS",
-    default=["strategistechconsulting.com"],
+    default=[".strategistechconsulting.com"],
 )
 
 # DATABASES
